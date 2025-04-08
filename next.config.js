9cbd73aed7df6fd187af4325b9f68d1d135ade56
Lewis Dutton <lewisdutton2@hotmail.com>
2025-04-08 20:03:48 +0100

    image optimisation

--- next.config.js
+++ next.config.js
@@ -2,70 +2,87 @@
 const nextConfig = {
   output: 'export',
   images: {
+    unoptimized: true,
     remotePatterns: [
       {
         protocol: 'https',
         hostname: 'upload.wikimedia.org',
+        pathname: '/**',
       },
       {
         protocol: 'https',
         hostname: 'images.unsplash.com',
+        pathname: '/**',
       },
       {
         protocol: 'https',
         hostname: 'encrypted-tbn0.gstatic.com',
+        pathname: '/**',
       },
       {
         protocol: 'https',
         hostname: 'static.designboom.com',
+        pathname: '/**',
       },
       {
         protocol: 'https',
         hostname: 'thebourbonconcierge.com',
+        pathname: '/**',
       },
       {
         protocol: 'https',
         hostname: 'robbreport.com',
+        pathname: '/**',
       },
       {
         protocol: 'https',
         hostname: 'cdn11.bigcommerce.com',
+        pathname: '/**',
       },
       {
         protocol: 'https',
         hostname: 'musubikiln.com',
+        pathname: '/**',
       },
       {
         protocol: 'https',
         hostname: 'i.etsystatic.com',
+        pathname: '/**',
       },
       {
         protocol: 'https',
         hostname: 'jeroenmarkies.co.uk',
+        pathname: '/**',
       },
       {
         protocol: 'https',
         hostname: 'cdn.shopify.com',
+        pathname: '/**',
       },
       {
         protocol: 'https',
         hostname: 'www.guggenheim.org',
+        pathname: '/**',
       },
       {
         protocol: 'https',
         hostname: 'd3rf6j5nx5r04a.cloudfront.net',
+        pathname: '/**',
       },
       {
         protocol: 'https',
         hostname: 'outland.art',
+        pathname: '/**',
       },
       {
         protocol: 'https',
         hostname: 'thehouseofwhisky.com',
+        pathname: '/**',
       },
       {
         protocol: 'https',
         hostname: 'i.seadn.io',
+        pathname: '/**',
       }
     ]
   },
